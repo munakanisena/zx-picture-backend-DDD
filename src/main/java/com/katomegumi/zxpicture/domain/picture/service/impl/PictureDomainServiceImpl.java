cc44e44f31253206d0c13baf633d9191a994cc19
munakanisena <1271626721@qq.com>
2025-04-22 20:58:16 +0800

    update

--- src/main/java/com/katomegumi/zxpicture/domain/picture/service/impl/PictureDomainServiceImpl.java
+++ src/main/java/com/katomegumi/zxpicture/domain/picture/service/impl/PictureDomainServiceImpl.java
@@ -459,7 +459,7 @@ public class PictureDomainServiceImpl implements PictureDomainService {
         if (ObjUtil.isNull(space)) {
           throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
         }
-        ThrowUtils.throwIf(space.getUserId().equals(loginUser.getId()),ErrorCode.NO_AUTH_ERROR);
+        ThrowUtils.throwIf(!space.getUserId().equals(loginUser.getId()),ErrorCode.NO_AUTH_ERROR);
         //查看空间全部图(要有主色调的图)
         List<Picture> pictureList = pictureRepository.lambdaQuery()
                 .eq(Picture::getSpaceId, spaceId)
